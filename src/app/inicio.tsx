a0f681c923cbd169db0dacba299eff90587a9ce2
AizenSousuke <kurono1807@gmail.com>
2025-05-04 18:06:49 -0500

    update bg-img

--- src/app/inicio.tsx
+++ src/app/inicio.tsx
@@ -6,13 +6,18 @@ const Inicio = ({ id, numberPhone }: Props) => {
   return (
     <section
       id={id}
-      className="relative bg-[url(/bg-inicio.webp)] bg-fixed bg-center bg-cover min-h-[100vh] pt-[80px] flex items-center justify-center"
+      className="relative min-h-[100vh] pt-[80px] flex items-center justify-center"
     >
+      <div className="absolute w-full h-full top-0 left-0">
+        <div className="bg-black/50 w-full h-[100vh] absolute top-0 left-0" />
+        <div className="bg-[url(/bg-inicio.webp)] bg-fixed bg-center bg-cover w-full h-full"></div>
+      </div>
+
       <div className="relative z-10 text-center text-white px-3 sm:px-10 md:px-0 w-full md:w-[600px] flex flex-col gap-10">
         <h1 className="text-2xl min-[400px]:text-3xl md:text-4xl leading-[1.5] font-bold">
           Más Wear Medellín – Comodidad y Frescura en Todo Momento
         </h1>
-        <p className="text-base md:text-lg text-white">
+        <p className="text-base md:text-lg">
           Bienvenido a Más Wear Medellín, la marca de ropa interior masculina
           diseñada para brindarte la máxima comodidad y frescura durante todo el
           día. Nuestra tecnología y materiales te permiten moverte con total
@@ -28,7 +33,6 @@ const Inicio = ({ id, numberPhone }: Props) => {
           Comprar Ahora
         </a>
       </div>
-      <div className="bg-black/50 w-full h-full absolute top-0" />
     </section>
   );
 };
